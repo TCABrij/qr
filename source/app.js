728d5a0faaab5018efb17293753b20d2d463b5cf
Brij Kishor <exreply7878@gmail.com>
2022-07-03 16:09:57 +0530

    add download qr feature

--- source/app.js
+++ source/app.js
@@ -1,7 +1,10 @@
 const Data = document.getElementById('data')
 const generateBtn = document.querySelector('.generate-btn')
+const downloadBtn = document.querySelector('#dbtn')
 
 generateBtn.addEventListener('click', generateQR)
+downloadBtn.addEventListener('click', downloadQR)
+
 
 function generateQR(){
     let data = Data.value
@@ -23,4 +26,25 @@ function appendQR(QRCode){
     let img = document.querySelector('#qrCode img')
     img.src = QRCode
     document.querySelector('#dbtn').setAttribute('data-hidden', 'false')
+}
+
+function downloadQR(){
+    const canvas = document.createElement('canvas')
+    const img = document.querySelector('.qr-section img')
+    canvas.height = img.height
+    canvas.width = img.width
+    const ctx = canvas.getContext('2d')
+
+    // fixing Tainted Canvases can't be exported error 
+    img.setAttribute('crossorigin', 'anonymouse')
+
+    // Drawing QR to canvas so that it can be downloaded
+    ctx.drawImage(img, 0, 0, canvas.height, canvas.width)
+
+    // downloading 
+    const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream")
+    const link = document.createElement('a')
+    link.download = "QrCode.png"
+    link.href = image
+    link.click()
 }
